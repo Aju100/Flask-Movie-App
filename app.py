adecd0b0231a4b6d87a7233a6a30812d0c460679
Aju100 <ajutamang10@outlook.com>
2020-04-20 17:07:43 +0545

    Fixes search queries

--- app.py
+++ app.py
@@ -7,25 +7,28 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+	return render_template('index.html')
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+	return render_template('about.html')
 
 @app.route('/search', methods=['POST'])
 def search():
-    """if POST, query movie api for data and return results."""
-    title=request.form['title']
-    jsonresp=searchreq(title)
-    results=jsonresp["Search"]
-    return render_template("search_results.html",results=results)
+	"""if POST, query movie api for data and return results."""
+	title=request.form['title']
+	try:
+		jsonresp=searchreq(title)
+		results=jsonresp["Search"]
+		return render_template("search_results.html",results=results)
+	except Exception as e:
+		return render_template("notfound.html"),404
 
 @app.errorhandler(404)
 def notfound(error):
-    return render_template('notfound.html'),404
+	return render_template('notfound.html'),404
 
 
 
 if __name__=="__main__":
-    app.run(debug=True)
+	app.run(debug=True)

666298b54e3686a23b0ff827dddb984c9d452af2
Aju100 <ajutamang10@outlook.com>
2020-04-22 12:56:47 +0545

    Fixes bugs

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
-from flask import Flask, escape, request, render_template
+from flask import Flask, escape, request, render_template,session,flash
 from forsearch import searchreq
 import json
 import requests
-
+from datetime import timedelta
 app = Flask(__name__)
+app.config["SESSION_PERMANENT"]=True
+
+#This is how much favourite movie data last
+app.config["PERMANENT_SESSION_LIFETIME"]=timedelta(days=31)
+
+'''
+##copy and run this code in python terminal
+from hashlib import sha256
+text=csaju.encode("utf-8")
+text=sha256(text).hexdigest()
+print(text)
+#output is d273fd202ae36a79dd36f160616859903861e535d49b44793b1d2930b05ff33a
+'''
+
+#and encoded in hexadecimal
+#for session
+app.secret_key="d273fd202ae36a79dd36f160616859903861e535d49b44793b1d2930b05ff33a"
 
 @app.route('/')
 def index():
@@ -24,9 +41,44 @@ def search():
 	except Exception as e:
 		return render_template("notfound.html"),404
 
-@app.route('/favourites')
+@app.route('/favourites',methods=['POST','GET'])
 def favourite():
-	return render_template('favourites.html')
+	if request.method=="GET":
+		results=session.get("fav")
+		print(results)
+		flash("Favourite movies last only for 31 days")
+		if results!=None:
+			return render_template('favourites.html',results=results)
+		else:
+			return '<h1>Sorry</h1>'
+
+	elif request.method=="POST":
+		poster=request.form["poster"]
+		Title=request.form["title"]
+		Year=request.form["year"]
+		imdbID=request.form["imdb"]
+		Type=request.form["type"]
+		#templating the movie as in result
+		currentmovie={
+			"Type":Type,
+			"Poster":poster,
+			"Title":Title,
+			"Year":Year,
+			"imdbID":imdbID
+		}
+		print(session)
+		try:
+			listoffav=session["fav"]
+			listoffav.append(currentmovie)
+
+		except:
+			listoffav=[]
+			listoffav.append(currentmovie)
+
+		session["fav"]=listoffav
+		
+		return (redirect(url_for("favourite")))
+
 
 @app.errorhandler(404)
 def notfound(error):
---
+++ static/localstorage.js
@@ -0,0 +1,28 @@
+function favadder(){
+    var xhr= new XMLHttpRequest();
+    
+    //and check if works in mobile or not
+    //change this during porduction
+    url="http://localhost:5000/favourite";
+    xhr.open('POST',url);
+    xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded;charset=UTF-8');
+   /* document.addEventListener("DOMContentLoaded", function(event) { 
+        });
+        */
+    var title=document.getElementById("title").textContent;
+    var year=document.getElementById("year").innerHTML;
+    var type=document.getElementById("type").textContent;
+    var imdb=document.getElementById("details").textContent;
+    var Poster=document.getElementById("card-img-top").src;
+
+    xhr.send(`title=${title}&year=${year}&type=${type}&imdb=${imdb}&poster=${Poster}`);
+    xhr.onreadystatechange=function(){
+        document.getElementById("click").value="Added to Favourite";
+        document.getElementById("click").readOnly = true;
+    };
+    
+}
+
+function flash(msg){
+
+}
